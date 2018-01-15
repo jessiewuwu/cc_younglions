5d57108181d72cbe60dbf8d4a755de350c0757b5
jessiewuwu <jwu004@gmail.com>
2018-01-15 09:59:16 -0500

    add footer to all pages

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.write('\
+  \
+  <footer class="page-footer">\
+      <div class="footer-copyright">\
+              © 2018 Chinatown Community Young Lions\
+      </div>\
+  </footer>\
+  \
+');
